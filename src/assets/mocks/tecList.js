22310f8d73a876bc55efce0068587ec332923202
WillBarbosa1 <dsilverfox98@gmail.com>
2022-03-31 16:54:42 -0300

    feat: assets

---
+++ src/assets/mocks/tecList.js
@@ -0,0 +1,56 @@
+export default [
+    {
+        name: "java",
+        description: "linguagem de programação",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original-wordmark.svg",
+        isActive: false,
+    },
+    {
+        name: "python",
+        description: "linguagem de programação",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original-wordmark.svg",
+        isActive: false,
+    },
+    {
+        name: "javascript",
+        description: "linguagem de programação",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/javascript/javascript-original.svg",
+        isActive: false,
+    },
+    {
+        name: "typescript",
+        description: "linguagem de programação",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/typescript/typescript-original.svg",
+        isActive: false,
+    },
+    {
+        name: "ruby",
+        description: "linguagem de programação",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/ruby/ruby-original.svg",
+        isActive: false,
+    },
+    {
+        name: "nodejs",
+        description: "backend",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/nodejs/nodejs-original-wordmark.svg",
+        isActive: false,
+    },
+    {
+        name: "go",
+        description: "backend",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/go/go-original.svg",
+        isActive: false,
+    },
+    {
+        name: "vuejs",
+        description: "framework frontend",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/vuejs/vuejs-original-wordmark.svg",
+        isActive: false,
+    },
+    {
+        name: "reactjs",
+        description: "biblioteca frontend",
+        icon: "https://raw.githubusercontent.com/devicons/devicon/master/icons/react/react-original-wordmark.svg",
+        isActive: false,
+    },
+]
